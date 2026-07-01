a87b8f565c85932f242f5dfcf96606e607cdc9fc
Nampally Venkatbabu <116674069+Nampallyvenkatbabu@users.noreply.github.com>
2026-07-01 12:00:53 +0530

    Time: 316 ms (51.55%), Space: 68.1 MB (22.89%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["bonus"] = 0
+
+    employees.loc[
+        (employees["employee_id"] %2 !=0) & (-employees["name"].str.startswith("M")),"bonus"
+    ] = employees["salary"]
+
+    return employees[["employee_id", "bonus"]].sort_values("employee_id")
+
+
+"""
+
+employees = pd.DataFrame({
+    "employee_id": [2, 3, 7, 8, 9],
+    "name": ["Meir", "Michael", "Addilyn", "Juan", "Kannon"],
+    "salary": [3000, 3800, 7400, 6100, 7700]
+})
+
+employees["bonus"] = 0
+
+employees.loc[
+    (employees["employee_id"] % 2 != 0) &
+    (~employees["name"].str.startswith("M")),
+    "bonus"
+] = employees["salary"]
+
+result = employees[["employee_id", "bonus"]].sort_values("employee_id")
+
+print(result)
+
+
+"""
