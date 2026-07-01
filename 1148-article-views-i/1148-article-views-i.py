b8a86b6eb7b46ec3fffe88162e504a9c9c67f100
Nampally Venkatbabu <116674069+Nampallyvenkatbabu@users.noreply.github.com>
2026-07-01 11:29:12 +0530

    Time: 271 ms (84.85%), Space: 67.4 MB (45.28%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.py
+++ 1148-article-views-i/1148-article-views-i.py
@@ -8,15 +8,17 @@ def article_views(views: pd.DataFrame) -> pd.DataFrame:
 """ Without function
 
 views = pd.DataFrame({
-    "article_id": [1, 1, 2, 2, 3],
-    "author_id": [3, 3, 7, 4, 4],
-    "viewer_id": [5, 3, 7, 4, 8],
+    "article_id": [1, 1, 2, 2, 4, 3, 3],
+    "author_id": [3, 3, 7, 7, 7, 4, 4],
+    "viewer_id": [5, 6, 7, 6, 1, 4, 4],
     "view_date": [
         "2019-08-01",
         "2019-08-02",
         "2019-08-01",
-        "2019-08-03",
-        "2019-08-04"
+        "2019-08-02",
+        "2019-07-22",
+        "2019-07-21",
+        "2019-07-21"
     ]
 })
 
